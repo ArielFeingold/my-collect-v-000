9668c353030aca29927363dac85fb1d4dd709423
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-13 15:31:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@
 def my_collect(collection)
   i = 0
   mod_collection = []
-  while i < array.length
-    yield(array[i])
+  while i < collection.length
+    yield(collection[i])
     mod_collection <<
     i += 1
   end
