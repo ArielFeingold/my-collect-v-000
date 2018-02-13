b53781570d1f3dd4d70f672357a3ba27d200e02c
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-13 15:29:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 
 def my_collect(collection)
   i = 0
+  mod_collection = []
   while i < array.length
-    binding.pry
     yield(array[i])
+    mod_collection <<
     i += 1
   end
+  mod_collection
 end
